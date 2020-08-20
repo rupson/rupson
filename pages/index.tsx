48d2a49ca5557c1e7c2e353b758d08a87b4fd911
rupson <44114899+rupson@users.noreply.github.com>
2020-08-20 21:49:39 +0100

    change root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,213 +1,138 @@
-import Head from 'next/head'
+import { NextPage } from 'next';
+import React from 'react';
+import styled from 'styled-components';
 
-export const Home = (): JSX.Element => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import { theme } from '../layout/theme';
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+const Title = styled.h1`
+    text-align: center;
+    margin-top: 0;
+    padding-top: 0.67em;
+`;
 
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
+const Card = styled.div`
+    padding: 15px 0 0 0;
+    border: 1px solid black;
+    border-radius: 20px;
+`;
 
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
+const CardTitle = styled.h3`
+    margin: 0 0 12px 0;
+    padding: 0 0 0 10%;
+`;
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+const CardBody = styled.div`
+    /* border: solid 1px green; */
+    > div:first-child {
+        border-top: solid 1px ${theme.colors.cardBorder};
+    }
+    > div:last-child {
+        border-bottom-right-radius: 20px;
+        border-bottom-left-radius: 20px;
+        border: none;
+    }
+`;
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+const CardsContainer = styled.div`
+    display: flex;
+    /* desktop view */
+    @media screen and (min-width: 769px) {
+        flex-direction: row;
+        justify-content: space-evenly;
         align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      .logo {
-        height: 1em;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
+        margin-top: 10%;
+        .card {
+            width: 40%;
         }
-      }
-    `}</style>
+    }
+    /* mobile view */
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        overflow: scroll;
+        .card {
+            width: 90%;
+            margin-top: 20px;
+        }
+    }
+`;
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+const SocialLinkContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    padding: 10px;
+    border-bottom: solid 1px ${theme.colors.cardBorder};
+    :hover {
+        background-color: ${theme.colors.linkHover};
+    }
+`;
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+const Anchor = styled.a`
+    width: 100%;
+    text-align: center;
+    text-decoration: none;
+    color: ${theme.colors.text};
+`;
 
-export default Home
+const Icon: React.FC<{ url: string; alt?: string }> = ({ url, alt }) => {
+    return <img src={url} alt={alt ? alt : ''} height={`24px`} />;
+};
+
+interface SocialLinkProps {
+    iconUrl: string;
+    linkText: string;
+    url: string;
+}
+const SocialLink: React.FC<SocialLinkProps> = ({ iconUrl, linkText, url }) => {
+    return (
+        <SocialLinkContainer>
+            <Icon url={iconUrl} />
+            <Anchor href={url}>{linkText}</Anchor>
+        </SocialLinkContainer>
+    );
+};
+
+const LandingPage: NextPage = () => {
+    return (
+        <>
+            <Title>rupson.me</Title>
+            <CardsContainer>
+                <Card className={'card'}>
+                    <CardTitle>💼 Professional</CardTitle>
+                    <CardBody>
+                        <SocialLink
+                            iconUrl={'/GitHub-Mark-32px.png'}
+                            url={'https://github.com/rupson'}
+                            linkText={`Github`}
+                        />
+                        <SocialLink
+                            iconUrl={'/LI-In-Bug.png'}
+                            url={'https://www.linkedin.com/in/rupson/'}
+                            linkText={`Linkedin`}
+                        />
+                    </CardBody>
+                </Card>
+                <Card className={'card'}>
+                    <CardTitle>🧸 Personal</CardTitle>
+                    <CardBody>
+                        <SocialLink
+                            iconUrl={'/glyph-logo_May2016.png'}
+                            url={'https://www.instagram.com/rupson22/'}
+                            linkText={'Instagram'}
+                        />
+                        <SocialLink
+                            iconUrl={'/Twitter_Logo_Blue.svg'}
+                            url={'https://twitter.com/rupson_'}
+                            linkText={'Twitter'}
+                        />
+                    </CardBody>
+                </Card>
+            </CardsContainer>
+        </>
+    );
+};
+
+export default LandingPage;
